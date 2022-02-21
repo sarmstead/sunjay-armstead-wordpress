06f267fcbd08361fc007c8fc37a212650687d4bf
Sunjay Armstead <connect@sunjayarmstead.local>
2022-02-21 08:05:01 -0500

    Initialize git repo with boilerplate Divi child code

---
+++ functions.php
@@ -0,0 +1,6 @@
+<?php
+function sunjay_armstead_enqueue_theme() {
+    wp_enqueue_style( 'parent-theme', get_template_directory_uri() . '/style.css' );
+    
+}
+add_action( 'wp_enqueue_scripts', 'sunjay_armstead_enqueue_theme' );
